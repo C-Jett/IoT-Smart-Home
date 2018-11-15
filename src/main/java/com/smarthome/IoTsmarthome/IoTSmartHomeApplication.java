319081d2f85aa1d06e87fc5f95d0f35355ae55f4
Jett, Cody <cody.jett@bbva.com>
2018-11-15 00:42:57 -0600

    Finished Spring Boot initialization. Fixed localhost and PostgreSQL connection.

--- src/main/java/com/smarthome/IoTsmarthome/IoTSmartHomeApplication.java
+++ src/main/java/com/smarthome/IoTsmarthome/IoTSmartHomeApplication.java
@@ -3,9 +3,7 @@ package com.smarthome.IoTsmarthome;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class IoTSmartHomeApplication {
--- src/main/java/com/smarthome/IoTsmarthome/controller/consumer_controller.java
+++ src/main/java/com/smarthome/IoTsmarthome/controller/consumer_controller.java
@@ -1,7 +1,9 @@
 package com.smarthome.IoTsmarthome.controller;
 
+import com.smarthome.IoTsmarthome.model.consumers;
 import com.smarthome.IoTsmarthome.repo.ConsumerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,14 +12,16 @@ public class consumer_controller {
     @Autowired
     private ConsumerRepository consumerRepository;
 
-    //Link for actually just listing all consumers. Will be replaced by consumers eventually.
-//    @RequestMapping("/findall")
-//    public String findAll() {
-//        String result = "";
-//        int i = 0;
-//
-////        for(consumers f : consumerRepository.findAll()){
-////            result =+ i + " + "
-////        }
-////    }
+    @RequestMapping("/")
+    public String findAll() {
+        String result = "";
+        int i = 0;
+
+        for (consumers f : consumerRepository.findAll()) {
+            result += i + " : " + f.toString() + "</br>";
+            i++;
+        }
+
+        return "result : " + result;
+    }
 }
